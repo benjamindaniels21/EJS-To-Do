7c24f2975c1db708b05db25abd34c929e11c8a95
Ben Daniels <benjamindaniels21@gmail.com>
2023-04-20 18:04:11 -0400

    more let and const

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let items = [];
-let workItems = [];
+const items = [];
+const workItems = [];
 
 app.get("/", (req, res) => {
   let day = date.getDate();
