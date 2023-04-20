5e99e379814431eeef48fb3f55cddfefa2717ed4
Ben Daniels <benjamindaniels21@gmail.com>
2023-04-20 08:25:11 -0400

    correct redirect after submit

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 let items = [];
+let workItems = [];
 
 app.get("/", (req, res) => {
   let today = new Date();
@@ -20,11 +21,31 @@ app.get("/", (req, res) => {
 
   let day = today.toLocaleDateString("en-US", options);
 
-  res.render("list", { kindOfDay: day, newListItems: items });
+  res.render("list", { listTitle: day, newListItems: items });
+});
+
+app.get("/work", (req, res) => {
+  res.render("list", { listTitle: "Work List", newListItems: workItems });
+});
+
+app.post("/work", (req, res) => {
+  let item = req.body.newItem;
+  workItems.push(item);
+
+  res.redirect("/work");
 });
 
 app.post("/", (req, res) => {
   let item = req.body.newItem;
+
+  if (req.body.list === "Work List") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+
   items.push(item);
 
   res.redirect("/");
